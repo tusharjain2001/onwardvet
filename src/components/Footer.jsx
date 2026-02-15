bda3e1c9f072c1bbeb82f72a04f75609ba2989f4
Tushar Jain <tushar.j@rrhaitech.com>
2026-02-16 00:36:10 +0530

    changes to boook demo and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,7 +29,7 @@ export default function Footer({
                 <img src={fbIconSrc} alt="Facebook" className="h-7 w-7" />
               </div>
 
-              <button className="mt-8 rounded-full bg-[#FF5A3D] px-10 py-4 text-base font-semibold text-white shadow-sm hover:opacity-95 active:opacity-90">
+              <button className="mt-8 bg-[#F86446] text-white px-5 sm:px-7 py-2.5 rounded-full text-sm sm:text-base font-medium whitespace-nowrap hover:opacity-90 transition">
                 Book A Demo
               </button>
             </div>
@@ -45,7 +45,7 @@ export default function Footer({
                 className="h-32 w-auto md:h-40 lg:h-44"
               />
 
-              <p className="mt-6 max-w-md text-center text-base font-semibold leading-6 text-zinc-700 md:text-lg">
+              <p className="mt-6 max-w-md text-center font-samsung text-base font-bold leading-6 text-[#3F3F3F] md:text-lg">
                 Modern Veterinary Software | Built-In
                 <br />
                 AI | Runs Your Entire Clinic | Easy to Use
@@ -56,8 +56,8 @@ export default function Footer({
       </div>
 
       {/* BOTTOM DARK BAR */}
-      <div className="bg-[#3E3E3E]">
-        <div className="mx-auto flex max-w-7xl flex-col items-center justify-between gap-3 px-6 py-5 text-sm text-white/80 md:flex-row">
+      <div className="bg-[#3F3F3F] inter-400">
+        <div className="mx-auto flex max-w-7xl flex-col  items-center justify-between gap-3 px-6 py-5 text-sm text-white/80 md:flex-row">
           <div>© {year} OnwardVet - All Rights Reserved</div>
 
           <div className="flex items-center gap-12">
